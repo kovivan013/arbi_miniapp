93872122f0e039b5803f377dc6e45c15f6c4d166
kovivan013 <kovivan013@gmail.com>
2024-12-06 14:28:51 +0200

    prod_version

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,37 +5,36 @@ function Menu() {
     return (
         <>
             <div className="menu nav">
-                <div className="menu div-btn">
-                    <button className="menu btn">
-                        <img src={reactLogo} alt="" />
-                        <p>Wallet</p>
-                    </button>
-                </div>
-                <div className="menu div-btn">
-                    <button className="menu btn">
-                        <img src={reactLogo} alt="" />
-                        <p>Friends</p>
-                    </button>
-                </div>
-                <div className="menu div-btn">
-                    <button className="menu btn">
-                        <img src={reactLogo} alt="" />
-                        <p>Home</p>
-                    </button>
-                </div>
-                <div className="menu div-btn">
-                    <button className="menu btn">
-                        <img src={reactLogo} alt="" />
-                        <p>Signals</p>
-                    </button>
-                </div>
-                <div className="menu div-btn">
-                    <button className="menu btn">
-                        <img src={reactLogo} alt="" />
-                        <p>More</p>
-                    </button>
-                </div>
+                <button className="menu btn">
+                    <img src={reactLogo} alt="" />
+                    <p>Wallet</p>
+                </button>
+                <button className="menu btn">
+                    <img src={reactLogo} alt="" />
+                    <p>Friends</p>
+                </button>
+                <button className="menu btn">
+                    <img src={reactLogo} alt="" />
+                    <p>Home</p>
+                </button>
+                <button className="menu btn">
+                    <img src={reactLogo} alt="" />
+                    <p>Signals</p>
+                </button>
+                <button className="menu btn">
+                    <img src={reactLogo} alt="" />
+                    <p>More</p>
+                </button>
             </div>
+            <nav className="menu">
+                <ul className="menu-list">
+                    <li className="menu-item"><a href="#">Главная</a></li>
+                    <li className="menu-item"><a href="#">О нас</a></li>
+                    <li className="menu-item"><a href="#">Услуги</a></li>
+                    <li className="menu-item"><a href="#">Цены</a></li>
+                    <li className="menu-item"><a href="#">Контакты</a></li>
+                </ul>
+            </nav>
         </>
     )
 }
