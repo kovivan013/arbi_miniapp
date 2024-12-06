13efc7d3864daa6d1a57b6609f818d3698e99986
kovivan013 <kovivan013@gmail.com>
2024-12-06 14:53:18 +0200

    prod_version

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,38 +1,15 @@
 import "../App.css"
-import reactLogo from '../assets/react.svg'
 
 function Menu() {
     return (
         <>
-            <div className="menu nav">
-                <button className="menu btn">
-                    <img src={reactLogo} alt="" />
-                    <p>Wallet</p>
-                </button>
-                <button className="menu btn">
-                    <img src={reactLogo} alt="" />
-                    <p>Friends</p>
-                </button>
-                <button className="menu btn">
-                    <img src={reactLogo} alt="" />
-                    <p>Home</p>
-                </button>
-                <button className="menu btn">
-                    <img src={reactLogo} alt="" />
-                    <p>Signals</p>
-                </button>
-                <button className="menu btn">
-                    <img src={reactLogo} alt="" />
-                    <p>More</p>
-                </button>
-            </div>
             <nav className="menu">
                 <ul className="menu-list">
-                    <li className="menu-item"><a href="#">Главная</a></li>
-                    <li className="menu-item"><a href="#">О нас</a></li>
-                    <li className="menu-item"><a href="#">Услуги</a></li>
-                    <li className="menu-item"><a href="#">Цены</a></li>
-                    <li className="menu-item"><a href="#">Контакты</a></li>
+                    <li className="menu-item"><a href="#">Wallet</a></li>
+                    <li className="menu-item"><a href="#">Friends</a></li>
+                    <li className="menu-item"><a href="#">Home</a></li>
+                    <li className="menu-item"><a href="#">Signals</a></li>
+                    <li className="menu-item"><a href="#">More</a></li>
                 </ul>
             </nav>
         </>
